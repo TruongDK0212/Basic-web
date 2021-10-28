b1a3309525ab707beaf9ebdf6409ddc89cabd2c7
truong <truong@gmail.com>
2021-10-28 10:09:05 +0700

    Hoàn thành chức năng thêm, xóa, sửa người dùng

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Lấy danh sách người dùng từ API
 try {
     requestCommon("GET", "users", null, function (status, res) {
         if (res) {
@@ -12,6 +13,8 @@ try {
 }
 
 var i = 0;  // id cho từng user trong bảng
+
+// Thể hiện danh sách người dùng ra bảng
 function insertTable(x) {
     var table = document.getElementById('addData');
     var s = '';
@@ -22,13 +25,15 @@ function insertTable(x) {
             <td>${e.lastName}</td>
             <td>${e.email}</td>
             <td><button type="button" class="btn btn-danger" onClick="removeUser('${e.id}', ${i})">Delete</button>
-            <button type="button" class="btn btn-primary" onClick="editUser('${e.id}', '${e.firstName}', '${e.lastName}', '${e.email}')" data-toggle="modal" data-target="#formEdit">Edit</button>
+            <button type="button" class="btn btn-primary" onClick="editUser('${e.id}', '${e.firstName}', '${e.lastName}', '${e.email}', ${i})" data-toggle="modal" data-target="#formEdit">Edit</button>
             </td>
             </tr>`;
         i++;
     });
     table.innerHTML = s;
 }
+
+// Kiểm tra trạng thái đăng nhập
 function checkLogin() {
     if (window.localStorage.getItem('is_login') == 'false') {
         window.location.href = "login.html";
@@ -50,7 +55,7 @@ function removeUser(id_user, i) {
     }
 }
 
-
+// Thêm mới người dùng
 function addUser() {
     var id = document.getElementById('id').value;
     var first_name = document.getElementById('fn').value;
@@ -84,17 +89,35 @@ function addUser() {
 
 
 // Hiển thị thông tin người dùng ra bảng sửa
-function editUser(id, fn , ln, email) {
+var current_i;
+function editUser(id, fn , ln, email, i) {
     document.getElementById('edit_id').value=id;
     document.getElementById('edit_fn').value=fn;
     document.getElementById('edit_ln').value=ln;
     document.getElementById('edit_email').value=email;
+    current_i = i;
 }
 
 // Lưu lại dữ liệu đã thay đổi
-// function saveChange(user_id) {
-//     var userInfor = {
+function saveChange(user_id) {
+    var userInfor = {
+        id: document.getElementById('edit_id').value,
+        firstName: document.getElementById('edit_fn').value,
+        lastName: document.getElementById('edit_ln').value,
+        email: document.getElementById('edit_email').value
+    }
+    requestCommon("PUT", "users/"+user_id, userInfor, function (status, res) {
+        alert("Trang thai: "+ status);
+    })
 
-//     }
-//     requestCommon("PUT", "users"+user_id, )
-// }
+    var newRow =`<tr class="user_${current_i}">
+    <td>`+document.getElementById('edit_id').value+`</td>
+    <td>`+document.getElementById('edit_fn').value+`</td>
+    <td>`+document.getElementById('edit_ln').value+`</td>
+    <td>`+document.getElementById('edit_email').value+`</td>
+    <td><button type="button" class="btn btn-danger" onClick="removeUser('${userInfor.id}', ${current_i})">Delete</button>
+    <button type="button" class="btn btn-primary" onClick="editUser('${userInfor.id}', '${userInfor.firstName}', '${userInfor.lastName}', '${userInfor.email}', ${current_i})" data-toggle="modal" data-target="#formEdit">Edit</button>
+    </td>
+    </tr>`;
+    $('tr.user_'+current_i).replaceWith(newRow);
+}
